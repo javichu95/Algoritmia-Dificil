dde9570b40521115ccf3d40869123726d6613619
Grexoj <george95zgz@gmail.com>
2017-01-03 22:01:33 +0100

    Fichero sh arreglado.

--- Practica 1/ejecutar1.sh
+++ Practica 1/ejecutar1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 javac practica1/*.java
 java practica1.Amazon -alg karger
-java practica1.Amazon -alg karger_stein
+java practica1.Amazon -alg karger_Stein
 java practica1.Amazon -alg karger_steinPesos
 java practica1.Amazon -alg kargerPesos
 java practica1.Amazon -ejemplo 1
